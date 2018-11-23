36dd0aa6f3e4d6306f6245f47e5ca21877a0db09
analgesic <38644641+gaoyx2016011900@users.noreply.github.com>
2018-11-23 11:16:50 +0800

    个人所得税计算器

---
+++ 2016011900gaoyanxin/taxcaculate/taxcaculate/Form_tax.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace taxcaculate
+{
+    partial class Form_tax
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lab_title = new System.Windows.Forms.Label();
+            this.lab_type = new System.Windows.Forms.Label();
+            this.lab_total = new System.Windows.Forms.Label();
+            this.lab_aftertax = new System.Windows.Forms.Label();
+            this.lab_tax = new System.Windows.Forms.Label();
+            this.txt_total = new System.Windows.Forms.TextBox();
+            this.txt_tax = new System.Windows.Forms.TextBox();
+            this.txt_afertax = new System.Windows.Forms.TextBox();
+            this.label_yuan2 = new System.Windows.Forms.Label();
+            this.label_yuan1 = new System.Windows.Forms.Label();
+            this.btn_compute = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.com_type = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lab_title
+            // 
+            this.lab_title.AutoSize = true;
+            this.lab_title.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_title.Location = new System.Drawing.Point(232, 54);
+            this.lab_title.Name = "lab_title";
+            this.lab_title.Size = new System.Drawing.Size(313, 37);
+            this.lab_title.TabIndex = 0;
+            this.lab_title.Text = "个人所得税计算器";
+            // 
+            // lab_type
+            // 
+            this.lab_type.AutoSize = true;
+            this.lab_type.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_type.Location = new System.Drawing.Point(119, 131);
+            this.lab_type.Name = "lab_type";
+            this.lab_type.Size = new System.Drawing.Size(120, 27);
+            this.lab_type.TabIndex = 1;
+            this.lab_type.Text = "收入类型";
+            // 
+            // lab_total
+            // 
+            this.lab_total.AutoSize = true;
+            this.lab_total.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_total.Location = new System.Drawing.Point(119, 184);
+            this.lab_total.Name = "lab_total";
+            this.lab_total.Size = new System.Drawing.Size(120, 27);
+            this.lab_total.TabIndex = 2;
+            this.lab_total.Text = "收入总额";
+            // 
+            // lab_aftertax
+            // 
+            this.lab_aftertax.AutoSize = true;
+            this.lab_aftertax.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_aftertax.Location = new System.Drawing.Point(119, 344);
+            this.lab_aftertax.Name = "lab_aftertax";
+            this.lab_aftertax.Size = new System.Drawing.Size(120, 27);
+            this.lab_aftertax.TabIndex = 3;
+            this.lab_aftertax.Text = "税后收入";
+            // 
+            // lab_tax
+            // 
+            this.lab_tax.AutoSize = true;
+            this.lab_tax.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_tax.Location = new System.Drawing.Point(119, 292);
+            this.lab_tax.Name = "lab_tax";
+            this.lab_tax.Size = new System.Drawing.Size(120, 27);
+            this.lab_tax.TabIndex = 3;
+            this.lab_tax.Text = "应缴税款";
+            // 
+            // txt_total
+            // 
+            this.txt_total.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_total.Location = new System.Drawing.Point(275, 184);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(149, 30);
+            this.txt_total.TabIndex = 4;
+            this.txt_total.TextChanged += new System.EventHandler(this.txt_total_TextChanged);
+            // 
+            // txt_tax
+            // 
+            this.txt_tax.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_tax.Location = new System.Drawing.Point(275, 292);
+            this.txt_tax.Name = "txt_tax";
+            this.txt_tax.Size = new System.Drawing.Size(149, 30);
+            this.txt_tax.TabIndex = 5;
+            // 
+            // txt_afertax
+            // 
+            this.txt_afertax.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_afertax.Location = new System.Drawing.Point(275, 344);
+            this.txt_afertax.Name = "txt_afertax";
+            this.txt_afertax.Size = new System.Drawing.Size(149, 30);
+            this.txt_afertax.TabIndex = 5;
+            // 
+            // label_yuan2
+            // 
+            this.label_yuan2.AutoSize = true;
+            this.label_yuan2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_yuan2.Location = new System.Drawing.Point(447, 344);
+            this.label_yuan2.Name = "label_yuan2";
+            this.label_yuan2.Size = new System.Drawing.Size(39, 27);
+            this.label_yuan2.TabIndex = 3;
+            this.label_yuan2.Text = "元";
+            // 
+            // label_yuan1
+            // 
+            this.label_yuan1.AutoSize = true;
+            this.label_yuan1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_yuan1.Location = new System.Drawing.Point(447, 292);
+            this.label_yuan1.Name = "label_yuan1";
+            this.label_yuan1.Size = new System.Drawing.Size(39, 27);
+            this.label_yuan1.TabIndex = 3;
+            this.label_yuan1.Text = "元";
+            // 
+            // btn_compute
+            // 
+            this.btn_compute.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_compute.Location = new System.Drawing.Point(275, 238);
+            this.btn_compute.Name = "btn_compute";
+            this.btn_compute.Size = new System.Drawing.Size(117, 27);
+            this.btn_compute.TabIndex = 6;
+            this.btn_compute.Text = "计算";
+            this.btn_compute.UseVisualStyleBackColor = true;
+            this.btn_compute.Click += new System.EventHandler(this.btn_compute_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_reset.Location = new System.Drawing.Point(435, 238);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(114, 27);
+            this.btn_reset.TabIndex = 7;
+            this.btn_reset.Text = "重置";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_click);
+            // 
+            // com_type
+            // 
+            this.com_type.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.com_type.FormattingEnabled = true;
+            this.com_type.Items.AddRange(new object[] {
+            "综合所得适用",
+            "经营所得适用"});
+            this.com_type.Location = new System.Drawing.Point(275, 131);
+            this.com_type.Name = "com_type";
+            this.com_type.Size = new System.Drawing.Size(149, 28);
+            this.com_type.TabIndex = 8;
+            this.com_type.Text = "收入类型选择";
+            this.com_type.SelectedIndexChanged += new System.EventHandler(this.com_index_change);
+            this.com_type.Click += new System.EventHandler(this.com_click);
+            // 
+            // Form_tax
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(696, 435);
+            this.Controls.Add(this.com_type);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_compute);
+            this.Controls.Add(this.txt_afertax);
+            this.Controls.Add(this.txt_tax);
+            this.Controls.Add(this.txt_total);
+            this.Controls.Add(this.lab_tax);
+            this.Controls.Add(this.label_yuan1);
+            this.Controls.Add(this.label_yuan2);
+            this.Controls.Add(this.lab_aftertax);
+            this.Controls.Add(this.lab_total);
+            this.Controls.Add(this.lab_type);
+            this.Controls.Add(this.lab_title);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Form_tax";
+            this.Text = "tax_caculate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab_title;
+        private System.Windows.Forms.Label lab_type;
+        private System.Windows.Forms.Label lab_total;
+        private System.Windows.Forms.Label lab_aftertax;
+        private System.Windows.Forms.Label lab_tax;
+        private System.Windows.Forms.TextBox txt_total;
+        private System.Windows.Forms.TextBox txt_tax;
+        private System.Windows.Forms.TextBox txt_afertax;
+        private System.Windows.Forms.Label label_yuan2;
+        private System.Windows.Forms.Label label_yuan1;
+        private System.Windows.Forms.Button btn_compute;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.ComboBox com_type;
+    }
+}
+
---
+++ 2016011900gaoyanxin/taxcaculate/taxcaculate/Form_tax.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taxcaculate
+{
+    public partial class Form_tax : Form
+    {
+        public double income;
+        public string input ;
+        //public double income= Convert.ToDouble(input);
+        public int typeindex;
+        public double income_after;
+        public double tax_output;
+        public int[][] domain = new int[2][]
+        {
+            new int[]{36000,144000,300000,420000,660000,960000,10000000},
+            new int[]{30000,90000,300000,500000,1000000}
+        };
+        public double[][] tax_rate = new double[2][]
+            {
+                new double[]{0.03,0.1,0.2,0.25,0.3,0.35,0.45},
+                new double[]{0.005,0.1,0.2,0.3,0.35}
+            }; 
+     
+        public Form_tax()
+        {
+            InitializeComponent();
+        }
+
+        public void tax(ref double tax_output,ref double income_after)
+        {
+            int i,j;
+            tax_output = 0;
+            income_after = 0;
+            income = Convert.ToDouble(txt_total.Text);
+           // MessageBox.Show(Convert.ToString( tax_rate[typeindex].Length));
+            for (i = tax_rate[typeindex].Length-1;i>=0 ; i--)
+                
+                if (income - domain[typeindex][i] > 0)
+                {
+                    tax_output = (income - domain[typeindex][i]) * tax_rate[typeindex][i+1];
+                    break;
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        tax_output = income * tax_rate[typeindex][0];
+                        break;
+                    }
+                    else 
+                    { continue; }
+
+                }
+
+            if (i > 0)
+            {
+                for (j = i; j >= 0; j--)
+                {
+                    MessageBox.Show(Convert.ToString(j), Convert.ToString(tax_output));
+                    tax_output = tax_output + (domain[typeindex][i] - domain[typeindex][i - 1]) * tax_rate[typeindex][i];
+
+                }
+            }
+
+            income_after = income - tax_output;
+           
+        }
+
+        private void btn_compute_Click(object sender, EventArgs e)
+        {
+            tax(ref tax_output, ref income_after);
+
+            //income_after = Convert.ToInt32(txt_total.Text);
+            txt_afertax.Text = Convert.ToString(income_after);
+            txt_tax.Text = Convert.ToString(tax_output);  
+        }
+
+        private void btn_reset_click(object sender, EventArgs e)
+        {
+            txt_total.Text = Convert.ToString(null);
+            txt_tax.Text = Convert.ToString(null);
+            txt_afertax.Text = Convert.ToString(null);
+        }
+
+        private void com_click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void com_index_change(object sender, EventArgs e)
+        {
+            typeindex = com_type.Items.IndexOf(com_type.Text);
+            //MessageBox.Show(com_type.Items[i].ToString());
+           /* switch(typeindex)
+            {
+                case 0:
+                    MessageBox.Show(com_type.Text);
+                    break;
+                case 1:
+                    MessageBox.Show(com_type.Text);
+                    break;
+           
+            }*/
+        }
+
+        private void txt_total_TextChanged(object sender, EventArgs e)
+        {
+            //string input;
+           // input = Console.ReadLine();
+            //MessageBox.Show(txt_total.Text);
+            /*income = Convert.ToDouble(txt_total.Text);
+            //MessageBox.Show(Convert.ToString(income));
+            */
+               
+        }
+
+    }
+}
