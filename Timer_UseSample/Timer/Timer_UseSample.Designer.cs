da006a7885448ea161dc867fb126d9ef493a472f
李少飞 <38090128+flyzerr@users.noreply.github.com>
2018-11-30 11:26:15 +0800

    Time_UseSample

---
+++ Timer_UseSample/Timer/Timer_UseSample.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Timer
+{
+    partial class Timer_UseSample
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1_UseSample = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.OutputBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // timer1_UseSample
+            // 
+            this.timer1_UseSample.Enabled = true;
+            this.timer1_UseSample.Interval = 1000;
+            this.timer1_UseSample.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(118, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(28, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "时间间隔";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(255, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "秒";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.FormattingEnabled = true;
+            this.OutputBox.Location = new System.Drawing.Point(27, 153);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(612, 290);
+            this.OutputBox.TabIndex = 6;
+            // 
+            // Timer_UseSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 462);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("宋体", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Timer_UseSample";
+            this.Text = "Timer_UseSample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1_UseSample;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox OutputBox;
+    }
+}
+
---
+++ Timer_UseSample/Timer/Timer_UseSample.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Timer_UseSample : Form
+    {
+
+        public Timer_UseSample()
+        {
+            InitializeComponent();
+        }
+
+        int temp = 0;//指定的间隔秒数
+        int pos = 0;//temp等0表示停止计时，等1表示开始计时
+        int i=0;    //临时变量
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            temp = Convert.ToInt32(textBox1.Text);  //获取间隔秒数
+            i = 0;
+            pos = 1;
+            this.OutputBox.Items.Add("开始计时");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pos = 0;
+            this.OutputBox.Items.Add("停止计时");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(pos==1)
+            {
+                i++;
+                if (i == temp)
+                {
+                    i = 0;
+                    this.OutputBox.Items.Add("-------定时器归零------");
+                    this.OutputBox.Items.Add("");
+                }
+                else
+                    this.OutputBox.Items.Add("临时变量：" + i.ToString());
+            }
+        }
+
+    }
+}
