724dab0b51ffc2b2dd4a361119c7164c0976e0cd
lunaplb <43959312+lunaplb@users.noreply.github.com>
2018-12-14 11:15:00 +0800

    enturst_001

--- 2016011932panlebing/entrust/entrust/Program.cs
+++ 2016011932panlebing/entrust/entrust/Program.cs
@@ -12,14 +12,13 @@ namespace entrust
         static void ProcessAndDisplayNumber(DoubleOp action, double value)
         {
             double result = action(value);
-            Console.WriteLine("Value is {0}, result of operation is {1}",
-                                    value, result);
+            Console.WriteLine("Value is {0}, result of operation is {1}", value, result);
+                                    
         }
 
         static void Main(string[] args)
         {
-            DoubleOp[] operations = { MathOperations.MultiplyByTwo,
-						MathOperations.Square };
+            DoubleOp[] operations = { MathOperations.MultiplyByTwo, MathOperations.Square };
             for (int i = 0; i < operations.Length; i++)
             {
                 Console.WriteLine("Using operations[{0}]:", i);
@@ -27,8 +26,9 @@ namespace entrust
                 ProcessAndDisplayNumber(operations[i], 7.94);
                 ProcessAndDisplayNumber(operations[i], 1.414);
                 Console.WriteLine();
-                Console.ReadLine();
+               
             }
+            Console.ReadLine();
         }
     }
     class MathOperations
