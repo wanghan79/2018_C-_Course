36fd3b79b212cc1e1403877d1eb3de89b6ac5c26
陈林峰 <38061732+chenlinfengliu@users.noreply.github.com>
2018-12-14 11:25:39 +0800

    regrate

---
+++ regate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate1
+{
+    class MathOperations
+    {
+        public static double MultiplyByTwo(double value)
+        {
+            return value * 2;
+        }
+        public static double Square(double value)
+        {
+            return value * value;
+        }
+
+    }
+    delegate double DoubleOp(double y);
+
+    class Program
+    {
+        static void ProcessAndDisplayNumber(DoubleOp action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine("Value is {0}, result of operation is {1}",value, result);
+        }
+
+        static void Main()
+        {
+      
+            DoubleOp[] operations = { MathOperations.MultiplyByTwo, MathOperations.Square };
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Using operations[{0}]:", i);
+                ProcessAndDisplayNumber(operations[i], 2.0);
+            }
+            Console.WriteLine();
+        }
+    }
+}
