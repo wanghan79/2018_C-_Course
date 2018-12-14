b568d6f14a1f6dd2a1a12f59d7bfb6c0a72efbfd
陈鹏宇 <37428331+2016011899chenpengyu@users.noreply.github.com>
2018-12-14 11:22:55 +0800

    Add files via upload

---
+++ Example 2/Example 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2
+{
+    class MathOperations
+    {
+        public static double MultiplyByTwo(double value)
+        {
+            return value * 2;
+        }
+        public static double Square(double value)
+        {
+            return value * value;
+        }
+    }
+    delegate double DoubleOp(double x);
+    class Program
+    {
+        static void ProcessAndDisplayNumber(DoubleOp action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine("Value is {0}, result of operation is {1}",value, result);
+        }
+        static void Main()
+        {
+            DoubleOp[] operations = { MathOperations.MultiplyByTwo,MathOperations.Square };
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine("Using operations[{0}]:", i);
+                ProcessAndDisplayNumber(operations[i], 2.0);
+                ProcessAndDisplayNumber(operations[i], 7.94);
+                ProcessAndDisplayNumber(operations[i], 1.414);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
