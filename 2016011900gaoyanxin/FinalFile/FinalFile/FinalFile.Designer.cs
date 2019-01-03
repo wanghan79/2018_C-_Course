71040197811a957b634422a23fb2ac0ce4fe0333
analgesic <38644641+gaoyx2016011900@users.noreply.github.com>
2019-01-04 07:13:50 +0800

    Final Work

---
+++ 2016011900gaoyanxin/FinalFile/FinalFile/FinalFile.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace FinalFile
+{
+    partial class FinalFile
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.tree_directory = new System.Windows.Forms.TreeView();
+            this.btn_choose = new System.Windows.Forms.Button();
+            this.txt_show = new System.Windows.Forms.TextBox();
+            this.fBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.lvw_show = new System.Windows.Forms.ListView();
+            this.col_filename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_changestyle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_changetime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_filetype = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tim = new System.Windows.Forms.Timer(this.components);
+            this.fSW = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fSW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_ok.Location = new System.Drawing.Point(480, 52);
+            this.btn_ok.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(57, 42);
+            this.btn_ok.TabIndex = 0;
+            this.btn_ok.Text = "ok";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btm_ok_Click);
+            // 
+            // tree_directory
+            // 
+            this.tree_directory.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tree_directory.LabelEdit = true;
+            this.tree_directory.Location = new System.Drawing.Point(59, 104);
+            this.tree_directory.Margin = new System.Windows.Forms.Padding(4);
+            this.tree_directory.Name = "tree_directory";
+            this.tree_directory.PathSeparator = "";
+            this.tree_directory.Size = new System.Drawing.Size(469, 418);
+            this.tree_directory.TabIndex = 0;
+            this.tree_directory.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.tree_directory_BeforeExpand);
+            this.tree_directory.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.tree_directory_AfterExpand);
+            this.tree_directory.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tree_directory_AfterSelect);
+            this.tree_directory.Layout += new System.Windows.Forms.LayoutEventHandler(this.tree_directory_Layout);
+            // 
+            // btn_choose
+            // 
+            this.btn_choose.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_choose.Location = new System.Drawing.Point(355, 51);
+            this.btn_choose.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_choose.Name = "btn_choose";
+            this.btn_choose.Size = new System.Drawing.Size(117, 45);
+            this.btn_choose.TabIndex = 1;
+            this.btn_choose.Text = "choose";
+            this.btn_choose.UseVisualStyleBackColor = true;
+            this.btn_choose.Click += new System.EventHandler(this.btn_choose_Click);
+            // 
+            // txt_show
+            // 
+            this.txt_show.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_show.Location = new System.Drawing.Point(59, 49);
+            this.txt_show.Multiline = true;
+            this.txt_show.Name = "txt_show";
+            this.txt_show.Size = new System.Drawing.Size(289, 47);
+            this.txt_show.TabIndex = 2;
+            this.txt_show.Text = "click \'choose\' to choose a file folder and then \'ok\'";
+            this.txt_show.TextChanged += new System.EventHandler(this.txt_show_TextChanged);
+            // 
+            // lvw_show
+            // 
+            this.lvw_show.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.col_filename,
+            this.col_changestyle,
+            this.col_changetime,
+            this.col_filetype});
+            this.lvw_show.Location = new System.Drawing.Point(636, 104);
+            this.lvw_show.Name = "lvw_show";
+            this.lvw_show.Scrollable = false;
+            this.lvw_show.Size = new System.Drawing.Size(445, 418);
+            this.lvw_show.TabIndex = 3;
+            this.lvw_show.UseCompatibleStateImageBehavior = false;
+            this.lvw_show.View = System.Windows.Forms.View.Details;
+            this.lvw_show.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lvw_show_ItemCheck);
+            this.lvw_show.SelectedIndexChanged += new System.EventHandler(this.lvw_show_SelectedIndexChanged);
+            // 
+            // col_filename
+            // 
+            this.col_filename.Tag = "";
+            this.col_filename.Text = "filename";
+            this.col_filename.Width = 70;
+            // 
+            // col_changestyle
+            // 
+            this.col_changestyle.Text = "changestyle";
+            this.col_changestyle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.col_changestyle.Width = 90;
+            // 
+            // col_changetime
+            // 
+            this.col_changetime.Text = "changetime";
+            this.col_changetime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.col_changetime.Width = 90;
+            // 
+            // col_filetype
+            // 
+            this.col_filetype.Text = "filetype";
+            this.col_filetype.Width = 90;
+            // 
+            // tim
+            // 
+            this.tim.Enabled = true;
+            this.tim.Interval = 10000;
+            this.tim.Tick += new System.EventHandler(this.tim_Tick);
+            
+            // 
+            // fSW
+            // 
+            this.fSW.EnableRaisingEvents = true;
+            this.fSW.SynchronizingObject = this;
+            this.fSW.Changed += new System.IO.FileSystemEventHandler(this.fSW_Changed);
+            // 
+            // FinalFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 563);
+            this.Controls.Add(this.lvw_show);
+            this.Controls.Add(this.txt_show);
+            this.Controls.Add(this.btn_choose);
+            this.Controls.Add(this.tree_directory);
+            this.Controls.Add(this.btn_ok);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FinalFile";
+            this.Text = "FinalFile";
+            this.Load += new System.EventHandler(this.FinalFile_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fSW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.TreeView tree_directory;
+        private System.Windows.Forms.Button btn_choose;
+        private System.Windows.Forms.TextBox txt_show;
+        private System.Windows.Forms.FolderBrowserDialog fBD;
+        private System.Windows.Forms.ListView lvw_show;
+        private System.Windows.Forms.ColumnHeader col_filename;
+        private System.Windows.Forms.ColumnHeader col_changestyle;
+        private System.Windows.Forms.ColumnHeader col_changetime;
+        private System.Windows.Forms.ColumnHeader col_filetype;
+        private System.Windows.Forms.Timer tim;
+        private System.IO.FileSystemWatcher fSW;
+    }
+}
+
---
+++ 2016011900gaoyanxin/FinalFile/FinalFile/FinalFile.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace FinalFile
+{
+ 
+    public partial class FinalFile : Form
+    {
+        public static string p = "";
+        public static string chosenPath="";
+        public static string[] path;
+        public static int flag=1;
+        
+        public FinalFile()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tree_directory_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+   
+        private void FinalFile_Load(object sender, EventArgs e)
+        {
+            if (chosenPath == "")
+            {
+
+                TreeNode rootNode = new TreeNode();  //载入显示 选择显示
+                rootNode.Tag = "我的电脑";                            //树节点数据
+                rootNode.Text = "我的电脑";                           //树节点标签内容
+                this.tree_directory.Nodes.Add(rootNode);               //树中添加根目录
+
+                //显示MyDocuments(我的文档)结点
+                var myDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                //myDocuments = "E:";
+                //MessageBox.Show(myDocuments );
+                TreeNode DocNode = new TreeNode(myDocuments);
+                DocNode.Tag = "我的文档";                            //设置结点名称
+                DocNode.Text = "我的文档";
+
+                rootNode.Nodes.Add(DocNode);                          //rootNode目录下加载节点
+                DocNode.Nodes.Add("");
+
+                //循环遍历计算机所有逻辑驱动器名称(盘符)
+                foreach (string drive in Environment.GetLogicalDrives())
+                {
+                    //实例化DriveInfo对象 命名空间System.IO
+                    var dir = new DriveInfo(drive);
+                    switch (dir.DriveType)           //判断驱动器类型
+                    {
+                        case DriveType.Fixed:        //仅取固定磁盘盘符 Removable-U盘 
+                            {
+                                //Split仅获取盘符字母
+                                TreeNode tNode = new TreeNode(dir.Name.Split(':')[0]);
+                                tNode.Name = dir.Name;
+                                tNode.Tag = tNode.Name;
+
+                                tree_directory.Nodes.Add(tNode);                    //加载驱动节点
+                                tNode.Nodes.Add("");
+                            }
+                            break;
+                    }
+                }
+                rootNode.Expand();                  //展开树状视图
+            }
+         
+
+        }
+
+        private void tree_directory_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void tree_directory_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            e.Node.Expand();
+        }
+
+        private void tree_directory_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeViewItems.Add(e.Node);
+        }
+        public static class TreeViewItems
+        {
+            public static void Add(TreeNode e)
+            {
+                //try..catch异常处理
+                //try..catch异常处理
+                try
+                {
+                    //判断"我的电脑"Tag 上面加载的该结点没指定其路径
+                    if (e.Tag.ToString() != "我的电脑")
+                    {
+                        e.Nodes.Clear();                               //清除空节点再加载子节点
+                        TreeNode tNode = e;                            //获取选中\展开\折叠结点
+                        string path = tNode.Name;                      //路径 
+                        if(flag==1)
+                        {
+                            path = chosenPath + path;
+                            flag = 0;
+                        }
+                        //MessageBox.Show();
+                        //获取"我的文档"路径
+                        if (e.Tag.ToString() == "我的文档")
+                        {
+                            path = Environment.GetFolderPath           //获取计算机我的文档文件夹
+                                (Environment.SpecialFolder.MyDocuments);
+                        }
+
+                        //获取指定目录中的子目录名称并加载结点GetDirectories
+                        string[] dics = Directory.GetFileSystemEntries(path);
+                        foreach (string dic in dics)
+                        {
+                            TreeNode subNode = new TreeNode(new DirectoryInfo(dic).Name); //实例化
+                            subNode.Name = new DirectoryInfo(dic).FullName;               //完整目录
+                            subNode.Tag = subNode.Name;
+                            
+                            tNode.Nodes.Add(subNode);
+                            subNode.Nodes.Add("");                               //加载空节点 实现+号
+                        }
+                    }
+                }
+                catch (Exception msg)
+                {
+                    MessageBox.Show(msg.Message);                   //异常处理
+                }
+            }
+        }
+
+        private void btn_choose_Click(object sender, EventArgs e)
+        {
+           /* OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel文件(*.xls;*.xlsx)|*.xls;*.xlsx|所有文件|*.*";
+            ofd.ValidateNames = true;
+            ofd.CheckPathExists = true;
+            ofd.CheckFileExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string strFileName = ofd.FileName;
+                //其他代码
+            }*/
+            if (fBD.ShowDialog() == DialogResult.OK)
+            {
+                txt_show.Text = fBD.SelectedPath;
+            }
+          
+        }
+
+        private void txt_show_TextChanged(object sender, EventArgs e)
+        {
+            flag = 1;
+            chosenPath=txt_show.Text;
+           // MessageBox.Show(path);
+            if(chosenPath!="")
+            {
+                  this.tree_directory.Nodes.Clear();
+                  path = chosenPath.Split('\\');
+                  //MessageBox.Show(path[path.Length-1]);
+                  TreeNode rNode = new TreeNode();  //载入显示 选择显示
+                  if (path.Length == 2)
+                  {
+                      rNode.Text = path[0] + path[1];
+                  }
+                  else {
+                      rNode.Text = path[path.Length - 1];
+                  }
+                  rNode.Tag = path[path.Length - 1];                            //树节点数据
+                                            //树节点标签内容
+                  this.tree_directory.Nodes.Add(rNode);               //树中添加根目录
+                  //rNode.Nodes.Add(rNode);
+                                          //rootNode目录下加载节点
+                  rNode.Nodes.Add("");
+                  rNode.Expand();
+            }
+        }
+
+        private void lvw_show_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+        }
+
+     
+
+        private void lvw_show_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btm_ok_Click(object sender, EventArgs e)
+        {
+          
+            MessageBox.Show("OK");
+               
+        }
+
+        private delegate void setLogTextDelegate(FileSystemEventArgs e);
+       
+        private void fileSystemWatcher_EventHandle(object sender, FileSystemEventArgs e)  //文件增删改时被调用的处理方法
+        {
+            
+            if (this.lvw_show.InvokeRequired)  //判断是否跨线程      
+            {           
+                this.lvw_show.Invoke(new setLogTextDelegate(setLogText), new object[] { e });   //使用委托将方法封送到UI主线程处理     
+            }
+            
+            
+        }
+        
+        private void tim_Tick(object sender, EventArgs e)
+        {
+            
+            lvw_show.Items.Clear();
+           
+            FileSystemWatcher fsw = new FileSystemWatcher();
+            
+            fsw.Path = chosenPath;
+            fsw.EnableRaisingEvents = true;  //启动监控
+            //fsw.IncludeSubdirectories = true;
+            fsw.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.Size;   //设置文件的文件名、文件写入及文件的大小改动会触发Changed事件 
+            fsw.Created += new FileSystemEventHandler(this.fileSystemWatcher_EventHandle);  //绑定事件触发后处理数据的方法。 
+            fsw.Deleted += this.fileSystemWatcher_EventHandle; 
+            fsw.Changed += this.fileSystemWatcher_EventHandle;
+            
+            
+            
+           // MessageBox.Show(f.Path);
+           
+
+        }
+        private void setLogText(FileSystemEventArgs e)  //更新UI界面
+        {
+            // FileInfo file = new FileInfo(e.FullPath);
+            p = e.FullPath;
+
+            FileInfo file = new FileInfo(p);
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = e.Name;   //受影响的文件名       
+            lvi.SubItems.Add(e.ChangeType.ToString());   //受影响文件的变动类型(可能为Created、Changed、Deleted）       
+            lvi.SubItems.Add("" + file.LastWriteTime);                     //受影响的文件修改时间
+            lvi.SubItems.Add(file.Extension);
+            lvw_show.Items.Add(lvi);
+           
+            
+        }
+
+        private void fSW_Changed(object sender, FileSystemEventArgs e)
+        {
+           
+        }
+    }
+
+}
+
