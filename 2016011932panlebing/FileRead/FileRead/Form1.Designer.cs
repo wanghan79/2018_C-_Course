dd9b597fb54868b372923d6c4a345692a3ca97f7
lunaplb <43959312+lunaplb@users.noreply.github.com>
2019-01-04 11:33:00 +0800

    Add files via upload

---
+++ 2016011932panlebing/FileRead/FileRead/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FileRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.butSubmit = new System.Windows.Forms.Button();
+            this.txtLoc = new System.Windows.Forms.TextBox();
+            this.labChoose = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.time = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(12, 62);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(138, 376);
+            this.treeView.TabIndex = 0;
+            // 
+            // butSubmit
+            // 
+            this.butSubmit.Location = new System.Drawing.Point(222, 27);
+            this.butSubmit.Name = "butSubmit";
+            this.butSubmit.Size = new System.Drawing.Size(49, 23);
+            this.butSubmit.TabIndex = 1;
+            this.butSubmit.Text = "确定";
+            this.butSubmit.UseVisualStyleBackColor = true;
+            this.butSubmit.MouseClick += new System.Windows.Forms.MouseEventHandler(this.butSubmit_MouseClick);
+            // 
+            // txtLoc
+            // 
+            this.txtLoc.Location = new System.Drawing.Point(116, 27);
+            this.txtLoc.Name = "txtLoc";
+            this.txtLoc.Size = new System.Drawing.Size(100, 21);
+            this.txtLoc.TabIndex = 2;
+            // 
+            // labChoose
+            // 
+            this.labChoose.AutoSize = true;
+            this.labChoose.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labChoose.Location = new System.Drawing.Point(30, 27);
+            this.labChoose.Name = "labChoose";
+            this.labChoose.Size = new System.Drawing.Size(80, 17);
+            this.labChoose.TabIndex = 3;
+            this.labChoose.Text = "选择的位置：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 4;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 12;
+            this.listBox.Location = new System.Drawing.Point(170, 62);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(395, 376);
+            this.listBox.TabIndex = 5;
+            // 
+            // time
+            // 
+            this.time.Tick += new System.EventHandler(this.time_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 452);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labChoose);
+            this.Controls.Add(this.txtLoc);
+            this.Controls.Add(this.butSubmit);
+            this.Controls.Add(this.treeView);
+            this.Name = "Form1";
+            this.Text = "文件读取显示";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button butSubmit;
+        private System.Windows.Forms.TextBox txtLoc;
+        private System.Windows.Forms.Label labChoose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Timer time;
+    }
+}
+
---
+++ 2016011932panlebing/FileRead/FileRead/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileRead
+{
+    public partial class Form1 : Form
+    {
+        TreeNode rootNode = new TreeNode();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void butSubmit_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            string fileLoc = txtLoc.Text;
+            //TreeNode rootNode = new TreeNode();
+            rootNode.Text = fileLoc;
+            treeView.Nodes.Add(rootNode);
+            director(fileLoc,rootNode);
+            WatcherStrat(fileLoc, "");
+            listBox.Items.Add("修改类型         时间            文件名" );
+            this.time.Enabled = true;
+        }
+        public void director(string dirs,TreeNode upNode)
+        {
+            
+            //绑定到指定的文件夹目录
+            DirectoryInfo dir = new DirectoryInfo(dirs);
+            //检索表示当前目录的文件和子目录
+            FileSystemInfo[] fsinfos = dir.GetFileSystemInfos();
+            //遍历检索的文件和子目录
+            foreach (FileSystemInfo fsinfo in fsinfos)
+            {
+                //判断是否为空文件夹　　
+                if (fsinfo is DirectoryInfo)
+                {
+                    //递归调用
+                    TreeNode sonNode = new TreeNode();
+                    sonNode.Text = fsinfo.FullName;
+                    upNode.Nodes.Add(sonNode);
+                    director(fsinfo.FullName, sonNode);
+                }
+                else
+                {
+                    TreeNode sonNode = new TreeNode();
+                    sonNode.Text = fsinfo.FullName;
+                    upNode.Nodes.Add(sonNode);
+                    
+                }
+            }
+        }
+        private void WatcherStrat(string path, string filter)
+       {
+ 
+           FileSystemWatcher watcher = new FileSystemWatcher();
+           watcher.Path = path;
+           watcher.Filter = filter;
+           watcher.Changed += new FileSystemEventHandler(OnProcess);
+           watcher.Created += new FileSystemEventHandler(OnProcess);
+           watcher.Deleted += new FileSystemEventHandler(OnProcess);
+           watcher.Renamed += new RenamedEventHandler(OnRenamed);
+           watcher.EnableRaisingEvents = true;
+           watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastAccess
+                                  | NotifyFilters.LastWrite | NotifyFilters.Security | NotifyFilters.Size;
+           watcher.IncludeSubdirectories = true;
+       }
+        private void OnProcess(object source, FileSystemEventArgs e)
+       {
+           if (e.ChangeType == WatcherChangeTypes.Created)
+           {
+               OnCreated(source, e);
+           }
+           else if (e.ChangeType == WatcherChangeTypes.Changed)
+           {
+               OnChanged(source, e);
+           }
+           else if (e.ChangeType == WatcherChangeTypes.Deleted)
+           {
+               OnDeleted(source, e);
+           }
+ 
+       }
+        private void OnCreated(object source, FileSystemEventArgs e)
+       {
+           FileInfo f = new FileInfo(e.FullPath);
+           string time = f.CreationTime.ToString();
+           listBox.Items.Add("  新建    " + time + "    " + e.Name);
+           this.time.Enabled = true;
+           
+       }
+       private void OnChanged(object source, FileSystemEventArgs e)
+       {
+           FileInfo f = new FileInfo(e.FullPath);
+           string time = f.LastWriteTime.ToString();
+           listBox.Items.Add("  修改    " + time + "    "  +e.Name);
+       }
+        
+       private void OnDeleted(object source, FileSystemEventArgs e)
+       {
+           string time = System.DateTime.Now.ToString();
+           listBox.Items.Add("  删除    " + time + "    " + e.Name);
+       }
+        
+       private void OnRenamed(object source, RenamedEventArgs e)
+       {
+           FileInfo f = new FileInfo(e.FullPath);
+           string time = f.LastAccessTime.ToString();
+           listBox.Items.Add("  重命名  " + time + "    " + e.Name);
+       }
+   
+        private void time_Tick(object sender, EventArgs e)
+        {
+            string fileLoc = txtLoc.Text;
+            //WatcherStrat(fileLoc, "");
+           
+        }
+    }
+}
