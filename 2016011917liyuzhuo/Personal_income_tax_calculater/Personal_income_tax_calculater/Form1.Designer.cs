6958b0cbd76e8e73802551dd98711705949eecec
李猛男 <37185276+2016011917liyuzhuo@users.noreply.github.com>
2019-01-04 10:35:14 +0800

    既有界面又有计时器
    反正发够三次了，问题不大

--- 2016011917liyuzhuo/Personal_income_tax_calculater/Personal_income_tax_calculater/Form1.Designer.cs
+++ 2016011917liyuzhuo/Personal_income_tax_calculater/Personal_income_tax_calculater/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btn_add = new System.Windows.Forms.Button();
             this.btn_calculate = new System.Windows.Forms.Button();
             this.btn_reset = new System.Windows.Forms.Button();
@@ -39,9 +40,10 @@
             this.txt_amount = new System.Windows.Forms.TextBox();
             this.txt_tax = new System.Windows.Forms.TextBox();
             this.txt_income = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmb_type = new System.Windows.Forms.ComboBox();
             this.lab_yuan1 = new System.Windows.Forms.Label();
             this.lab_yuan2 = new System.Windows.Forms.Label();
+            this.timer_printtxt = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // btn_add
@@ -59,6 +61,7 @@
             // btn_calculate
             // 
             this.btn_calculate.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_calculate.ForeColor = System.Drawing.Color.Black;
             this.btn_calculate.Location = new System.Drawing.Point(195, 254);
             this.btn_calculate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_calculate.Name = "btn_calculate";
@@ -78,6 +81,7 @@
             this.btn_reset.TabIndex = 2;
             this.btn_reset.Text = "重置";
             this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
             // 
             // lab_type
             // 
@@ -85,7 +89,7 @@
             this.lab_type.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lab_type.Location = new System.Drawing.Point(27, 129);
             this.lab_type.Name = "lab_type";
-            this.lab_type.Size = new System.Drawing.Size(88, 25);
+            this.lab_type.Size = new System.Drawing.Size(69, 20);
             this.lab_type.TabIndex = 3;
             this.lab_type.Text = "收入类型";
             // 
@@ -95,7 +99,7 @@
             this.lab_amount.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lab_amount.Location = new System.Drawing.Point(27, 200);
             this.lab_amount.Name = "lab_amount";
-            this.lab_amount.Size = new System.Drawing.Size(88, 25);
+            this.lab_amount.Size = new System.Drawing.Size(69, 20);
             this.lab_amount.TabIndex = 4;
             this.lab_amount.Text = "收入总额";
             // 
@@ -105,7 +109,7 @@
             this.lab_tax.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lab_tax.Location = new System.Drawing.Point(27, 333);
             this.lab_tax.Name = "lab_tax";
-            this.lab_tax.Size = new System.Drawing.Size(88, 25);
+            this.lab_tax.Size = new System.Drawing.Size(69, 20);
             this.lab_tax.TabIndex = 5;
             this.lab_tax.Text = "应纳税款";
             // 
@@ -115,7 +119,7 @@
             this.lab_income.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lab_income.Location = new System.Drawing.Point(27, 413);
             this.lab_income.Name = "lab_income";
-            this.lab_income.Size = new System.Drawing.Size(88, 25);
+            this.lab_income.Size = new System.Drawing.Size(69, 20);
             this.lab_income.TabIndex = 6;
             this.lab_income.Text = "税后收入";
             // 
@@ -125,7 +129,7 @@
             this.lab_title.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lab_title.Location = new System.Drawing.Point(145, 50);
             this.lab_title.Name = "lab_title";
-            this.lab_title.Size = new System.Drawing.Size(257, 40);
+            this.lab_title.Size = new System.Drawing.Size(206, 31);
             this.lab_title.TabIndex = 7;
             this.lab_title.Text = "个人所得税计算器";
             // 
@@ -134,45 +138,42 @@
             this.txt_amount.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txt_amount.Location = new System.Drawing.Point(152, 197);
             this.txt_amount.Name = "txt_amount";
-            this.txt_amount.Size = new System.Drawing.Size(208, 31);
+            this.txt_amount.Size = new System.Drawing.Size(208, 27);
             this.txt_amount.TabIndex = 8;
+            this.txt_amount.TextChanged += new System.EventHandler(this.txt_amount_TextChanged);
             // 
             // txt_tax
             // 
             this.txt_tax.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txt_tax.Location = new System.Drawing.Point(152, 327);
             this.txt_tax.Name = "txt_tax";
-            this.txt_tax.Size = new System.Drawing.Size(208, 31);
+            this.txt_tax.ReadOnly = true;
+            this.txt_tax.Size = new System.Drawing.Size(208, 27);
             this.txt_tax.TabIndex = 9;
+            this.txt_tax.TextChanged += new System.EventHandler(this.txt_tax_TextChanged);
             // 
             // txt_income
             // 
             this.txt_income.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txt_income.Location = new System.Drawing.Point(152, 407);
             this.txt_income.Name = "txt_income";
-            this.txt_income.Size = new System.Drawing.Size(208, 31);
+            this.txt_income.ReadOnly = true;
+            this.txt_income.Size = new System.Drawing.Size(208, 27);
             this.txt_income.TabIndex = 10;
+            this.txt_income.TextChanged += new System.EventHandler(this.txt_income_TextChanged);
             // 
-            // comboBox1
+            // cmb_type
             // 
-            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.comboBox1.Items.AddRange(new object[] {
-            "工资薪金",
-            "个体工商户生产经营",
-            "对企事业单位承包经营",
-            "承租经营",
-            "劳务报酬",
-            "稿酬",
-            "特许权使用费",
-            "利息股息红利",
-            "财产租赁",
-            "财产转让",
-            "偶然所得",
-            "其他"});
-            this.comboBox1.Location = new System.Drawing.Point(152, 122);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(208, 32);
-            this.comboBox1.TabIndex = 11;
+            this.cmb_type.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmb_type.Items.AddRange(new object[] {
+            "综合所得",
+            "经营所得",
+            "利息、股息、红利所得，财产租赁所得，财产转让所得和偶然所得"});
+            this.cmb_type.Location = new System.Drawing.Point(152, 122);
+            this.cmb_type.Name = "cmb_type";
+            this.cmb_type.Size = new System.Drawing.Size(208, 28);
+            this.cmb_type.TabIndex = 11;
+            this.cmb_type.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // lab_yuan1
             // 
@@ -180,7 +181,7 @@
             this.lab_yuan1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lab_yuan1.Location = new System.Drawing.Point(378, 333);
             this.lab_yuan1.Name = "lab_yuan1";
-            this.lab_yuan1.Size = new System.Drawing.Size(31, 25);
+            this.lab_yuan1.Size = new System.Drawing.Size(24, 20);
             this.lab_yuan1.TabIndex = 12;
             this.lab_yuan1.Text = "元";
             // 
@@ -190,18 +191,24 @@
             this.lab_yuan2.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lab_yuan2.Location = new System.Drawing.Point(378, 413);
             this.lab_yuan2.Name = "lab_yuan2";
-            this.lab_yuan2.Size = new System.Drawing.Size(31, 25);
+            this.lab_yuan2.Size = new System.Drawing.Size(24, 20);
             this.lab_yuan2.TabIndex = 13;
             this.lab_yuan2.Text = "元";
             // 
+            // timer_printtxt
+            // 
+            this.timer_printtxt.Enabled = true;
+            this.timer_printtxt.Interval = 1000;
+            this.timer_printtxt.Tick += new System.EventHandler(this.timer_printtxt_Tick);
+            // 
             // Form_PersonalIncomeTaxCalculator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(539, 526);
             this.Controls.Add(this.lab_yuan2);
             this.Controls.Add(this.lab_yuan1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmb_type);
             this.Controls.Add(this.txt_income);
             this.Controls.Add(this.txt_tax);
             this.Controls.Add(this.txt_amount);
@@ -236,9 +243,10 @@
         private System.Windows.Forms.TextBox txt_amount;
         private System.Windows.Forms.TextBox txt_tax;
         private System.Windows.Forms.TextBox txt_income;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmb_type;
         private System.Windows.Forms.Label lab_yuan1;
         private System.Windows.Forms.Label lab_yuan2;
+        private System.Windows.Forms.Timer timer_printtxt;
     }
 }
 
--- 2016011917liyuzhuo/Personal_income_tax_calculater/Personal_income_tax_calculater/Form1.cs
+++ 2016011917liyuzhuo/Personal_income_tax_calculater/Personal_income_tax_calculater/Form1.cs
@@ -7,11 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Personal_income_tax_calculater
 {
     public partial class Form_PersonalIncomeTaxCalculator : Form
     {
+        double income;
         public Form_PersonalIncomeTaxCalculator()
         {
             InitializeComponent();
@@ -19,7 +20,7 @@ namespace Personal_income_tax_calculater
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void Form_PersonalIncomeTaxCalculator_Load(object sender, EventArgs e)
@@ -29,7 +30,114 @@ namespace Personal_income_tax_calculater
 
         private void btn_calculate_Click(object sender, EventArgs e)
         {
+            txt_tax_TextChanged(null, null);
+            txt_income_TextChanged(null,null);
+        }
 
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_amount_TextChanged(object sender, EventArgs e)
+        {     
+
+        }
+        
+
+        public double txtConvertToDouble(string txt)
+        {
+            double result = 0;
+            bool f = double.TryParse(txt, out result);
+            return result;
+        }
+        
+
+        private void txt_income_TextChanged(object sender, EventArgs e)
+        {
+            income = txtConvertToDouble(txt_amount.Text);
+            double print=5000.0;
+                print = Incomeoutput(income);
+            txt_income.Text = print.ToString();
+                
+        }
+        public double Type_Comprehensive(double income)
+        {
+            double p = 0.0;
+            if (income <= 5000) p = 0.0;
+            else if (income <= 36000.0) p = (income - 5000) * 0.03;
+            else if (income > 36000 & income <= 144000) p = (income - 36000) * 0.1 + 31000 * 0.03;
+            else if (income > 144000 & income <= 300000.0) p = (income - 144000) * 0.2 + (144000 - 36000) * 0.1 + 31000 * 0.03;
+            else if ((income > 300000) & (income <= 420000)) p = (income - 300000) * 0.25 + (300000 - 144000) * 0.2 + (144000 - 36000) * 0.1 + 31000 * 0.03;
+            else if ((income > 420000) & (income <= 660000)) p = (income - 420000) * 0.3 + (420000 - 300000) * 0.25 + (300000 - 144000) * 0.2 + (144000 - 36000) * 0.1 + 31000 * 0.03;
+            else if ((income > 660000) & (income <= 960000)) p = (income - 660000) * 0.35 + (660000 - 420000) * 0.3 + (420000 - 300000) * 0.25 + (300000 - 144000) * 0.2 + (144000 - 36000) * 0.1 + 31000 * 0.03;
+            else p = (income - 960000) * 0.45 + (960000 - 660000) * 0.35 + (660000 - 420000) * 0.3 + (420000 - 300000) * 0.25 + (300000 - 144000) * 0.2 + (144000 - 36000) * 0.1 + 31000 * 0.03;
+            return p;
+        }
+        public double Type_Operating(double income)
+        {
+            double p = 0.0;
+            if (income <= 5000) p = 0.0;
+            else if (income <= 30000) p = (income - 5000) * 0.05;
+            else if (income <= 90000) p = (income - 30000) * 0.1+25000 * 0.05;
+            else if (income <= 300000) p = (income - 90000) * 0.2 +(9000-30000)*0.1 +25000 * 0.05;
+            else if (income <= 500000) p = (income - 300000) * 0.25+(300000-90000)*0.2+60000*0.1 + 25000 * 0.05;
+            else p = (income - 500000) * 0.3+200000*0.25+210000*0.2+60000*0.1 + 25000 * 0.05;
+            return p;
+        }
+        public double Type_Others(double income)
+        {
+            double p = 0.0;
+            p = income * 0.2;
+            return p;
+        }
+        public double Taxoutput(double income)
+        {
+            double p=0.0;
+            if (cmb_type.Text.ToString() == "综合所得") p = Type_Comprehensive(income);
+            else if (cmb_type.Text.ToString() == "经营所得") p = Type_Operating(income);
+            else p = Type_Others(income);
+            return p;
+        }
+        public double Incomeoutput(double income)
+        {
+            double p = income;
+            p = income - Taxoutput(income);
+            return p;
+        }
+        private void txt_tax_TextChanged(object sender, EventArgs e)
+        {
+            income = txtConvertToDouble(txt_amount.Text);
+            double print=0.0;
+            print = Taxoutput(income);
+            txt_tax.Text=print.ToString();
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            String nullstr=" ";
+            txt_amount.Text = nullstr.ToString();
+            txt_tax.Text = nullstr.ToString();
+            txt_income.Text = nullstr.ToString();
+                
+        }
+        int temp = 30;
+        int i = 0;
+        private void timer_printtxt_Tick(object sender, EventArgs e)
+        {
+          
+            i++;
+            if(i==temp) 
+            { 
+                OpenFileDialog f = new OpenFileDialog();
+                f.ShowDialog();
+
+            }
+        }
+
+        private void btn_startTimer_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
