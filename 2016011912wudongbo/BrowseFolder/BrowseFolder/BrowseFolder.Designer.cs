3d198207078c0009bed1da333f01cb1fcd927e54
2016011912wudongbo <37185420+2016011912wudongbo@users.noreply.github.com>
2019-01-16 20:50:51 +0800

    浏览文件夹确定路径的方法
    浏览文件夹确定路径的方法

---
+++ 2016011912wudongbo/BrowseFolder/BrowseFolder/BrowseFolder.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BrowseFolder
+{
+    partial class BrowseFolder
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.lblFilepath = new System.Windows.Forms.Label();
+            this.lbltitle = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(144, 204);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(918, 25);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // lblFilepath
+            // 
+            this.lblFilepath.AutoSize = true;
+            this.lblFilepath.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblFilepath.Location = new System.Drawing.Point(75, 203);
+            this.lblFilepath.Name = "lblFilepath";
+            this.lblFilepath.Size = new System.Drawing.Size(51, 20);
+            this.lblFilepath.TabIndex = 1;
+            this.lblFilepath.Text = "路径";
+            // 
+            // lbltitle
+            // 
+            this.lbltitle.AutoSize = true;
+            this.lbltitle.Font = new System.Drawing.Font("黑体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbltitle.Location = new System.Drawing.Point(290, 31);
+            this.lbltitle.Name = "lbltitle";
+            this.lbltitle.Size = new System.Drawing.Size(235, 24);
+            this.lbltitle.TabIndex = 2;
+            this.lbltitle.Text = "浏览文件夹选择路径";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSearch.Location = new System.Drawing.Point(1077, 204);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(142, 30);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "浏览文件夹";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // BrowseFolder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 590);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lbltitle);
+            this.Controls.Add(this.lblFilepath);
+            this.Controls.Add(this.txtFilePath);
+            this.Name = "BrowseFolder";
+            this.Text = "文件夹浏览";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label lblFilepath;
+        private System.Windows.Forms.Label lbltitle;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ 2016011912wudongbo/BrowseFolder/BrowseFolder/BrowseFolder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowseFolder
+{
+    public partial class BrowseFolder : Form
+    {
+        public BrowseFolder()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowDialog();
+            txtFilePath.Text = folderDlg.SelectedPath;
+        }
+    }
+}
